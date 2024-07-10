7e770f4993cbd7ef5e80518abd26bacd99f37ee6
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2024-07-11 02:28:02 +0300

    fix

--- src/components/UI/ModalImage/ModalImage.jsx
+++ src/components/UI/ModalImage/ModalImage.jsx
@@ -10,6 +10,7 @@ const customStyles = {
     left: '50%',
     right: 'auto',
     bottom: 'auto',
+    marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     borderRadius: '8px',
     border: 'none',
